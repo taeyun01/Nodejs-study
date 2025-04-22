c42a7e9c24541f076f667b9f08b001f4002c1829
taeyun <lwe12f675@gmail.com>
2025-04-23 02:51:18 +0900

    Express.js: Response (응답에 대해서 알아보기)

--- 6-express/app.js
+++ 6-express/app.js
@@ -20,6 +20,14 @@ app.get("/by/:id", (req, res, next) => {
   res.send(`요청한 쿼리는 ${req.params.id}번의 "${req.query.query}" 입니다.`);
 });
 
+app.get("/res", (req, res, next) => {
+  // res.send("Hi!"); // send로 데이터를 보낼 수 있음
+  // res.json({ myName: "Taeyun", myAge: 20 }); // json으로 데이터를 보낼 수 있음
+  // res.sendStatus(400); // Status 코드로 응답
+  res.setHeader("key", "hahahaha"); // 헤더에 특정한 값을 설정할 수 있음
+  res.status(201).send("Created"); // 처리된 결과에 따라 상태코드를 보내고 원하는 데이터를 보낼 수 있음
+});
+
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
