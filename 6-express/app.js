fa2b2185a20f349bedfb3d8079be72110b9a41eb
taeyun <lwe12f675@gmail.com>
2025-04-23 02:01:32 +0900

    Express.js: Request (요청에 대해서 알아보기)

--- 6-express/app.js
+++ 6-express/app.js
@@ -3,8 +3,21 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res, next) => {
-  console.log("요청한 메서드: ", req.method);
-  res.send("hello world"); // 응답을 해주지 않으면 브라우저에서 계속 대기 상태가 됨
+  // console.log("요청한 경로: ", req.path);
+  // console.log("요청한 헤더: ", req.headers);
+  // console.log("요청한 파라미터: ", req.params);
+  // console.log("요청한 쿼리: ", req.query);
+  res.send("hello world");
+});
+
+app.get("/hi/:id", (req, res, next) => {
+  console.log("요청한 파라미터: ", req.params);
+  res.send(`hi의 id는 ${req.params.id} 입니다.`);
+});
+
+app.get("/by/:id", (req, res, next) => {
+  console.log("요청한 쿼리: ", req.query);
+  res.send(`요청한 쿼리는 ${req.params.id}번의 "${req.query.query}" 입니다.`);
 });
 
 app.listen(8080, () => {
